2faa0ed02135d28446518db58bc232850f70d6d6
ysw <301094458@qq.com>
2020-08-06 11:20:03 +0800

    Initial commit

---
+++ src/main/java/com/wrx/LibrarywrxApplication.java
@@ -0,0 +1,13 @@
+package com.wrx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrarywrxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibrarywrxApplication.class, args);
+    }
+
+}
